217eca9b5e6da7eaa3d504765f1610ceaaa3a159
Naveed Waddo <naveedwaddo1123@gmail.com>
2024-11-04 20:15:03 +0530

    first commit

---
+++ ChainOfResponsiblity/src/com/naveed/chainofresponsibility/ChainOfRespATMExample.java
@@ -0,0 +1,10 @@
+package com.naveed.chainofresponsibility;
+
+public class ChainOfRespATMExample {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ DesignVendingMachine/src/vendingSates/Main.java
@@ -0,0 +1,10 @@
+package vendingSates;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
